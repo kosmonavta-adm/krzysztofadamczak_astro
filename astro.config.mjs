47489d5c6b4021e65fba3674ae44c0bd8d72f393
Krzysztof <krzadamczak@gmail.com>
2024-07-17 11:13:05 +0200

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/static';
-
 import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), mdx()],
-    // devToolbar: {
-    //     enabled: false,
-    // },
+    integrations: [tailwind(), mdx(), sitemap()],
+    devToolbar: {
+        enabled: false,
+    },
     prefetch: true,
+    site: 'https://krzysztofadamczak.pl/',
     adapter: vercel({
         webAnalytics: {
             enabled: true,
